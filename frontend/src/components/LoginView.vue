74bb329e586af6e7d2f9a6cbbf4b2dafcfd4e144
Wunderwaffel <37752072+Wunderwaffel@users.noreply.github.com>
2025-11-10 11:16:08 +0100

    Example implementation of API-Requests in frontend (#8)
    * Example implementation of API-Requests in frontend
    
    * Remove invalid package.json dependency

---
+++ frontend/src/components/LoginView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+
+const username = ref('michael');
+const errorMessage = ref(null);
+const isLoading = ref(false);
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const handleLogin = async () => {
+  isLoading.value = true;
+  errorMessage.value = null;
+
+  try {
+    console.log("Trying to login")
+    const response = await axios.post('http://localhost:8000/auth/login', {
+      username: username.value,
+    });
+    console.log(response)
+
+    const { access_token, refresh_token, user } = response.data;
+
+    authStore.setAuth({
+      accessToken: access_token,
+      refreshToken: refresh_token,
+      user,
+    });
+
+    console.log(authStore.accessToken);
+
+    await router.push({ name: 'home' });
+
+  } catch (error) {
+    if (error.response?.data?.detail) {
+      errorMessage.value = error.response.data.detail;
+    } else {
+      errorMessage.value = 'An unexpected error occurred.';
+    }
+    console.error('Login failed:', error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+</script>
+
+<template>
+  <div>
+    <v-btn @click="handleLogin" :disabled="isLoading || authStore.isAuthenticated">
+      {{ isLoading ? 'Logging in...' : 'Login' }}
+    </v-btn>
+  </div>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,25 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import 'vuetify/styles';
+import router from './router';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    theme: {
+        defaultTheme: 'light',
+    },
+});
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router);
+app.use(vuetify);
+
+app.mount('#app');
